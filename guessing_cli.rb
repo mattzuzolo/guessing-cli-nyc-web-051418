dc545c7e4110a430bbf38d7d139c7f2c23c6d601
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-02 17:38:51 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,59 +4,68 @@ def run_guessing_game
 
   user_guess = ""
   puts "Guess a number between 1 and 6."
-  user_guess = user_input
-  num = generate_random
   
+  user_guess = gets.downcase.chomp
+  random_num = rand(1..6).to_s
   
-
-
-
-  if user_guess == 'exit'
-    exit_program
-    #break
-  else  
-    check_guess
-  end  
-  
-end
-  
-
-def user_input
-  #puts "Guess a number or type 'exit' to leave"
-  user_input = gets.downcase.chomp
-end
-  
-def generate_random
-  rand(1..6).to_s #second number inclusive
-end
-
-def exit_program
-    puts "Goodbye"
-end
-  
-  
-  
-
-
-
-
-
-
-#compare input to a randomly generated number
-  #print out statement if correct
-  #print out statement if incorrectly
-def check_guess (user_guess, num)
-  
-  if user_guess == num
+  if user_guess == random_num
     puts "You guessed the correct number!"
+  elsif user_guess == "exit"
+    puts "Goodbye"
+    break
   else
     puts "The computer guessed #{num}."
-    exit_choice = gets.chomp
-    if exit_choice.downcase == "exit"
-      exit_program
-    end  
+  end  
+end  
+
+
+
+#   if user_guess == 'exit'
+#     exit_program
+#     #break
+#   else  
+#     check_guess
+#   end  
   
-end
+# end
+  
+
+# def user_input
+#   #puts "Guess a number or type 'exit' to leave"
+#   user_input = 
+# end
+  
+# def generate_random
+#   #second number inclusive
+# end
+
+# def exit_program
+#     puts "Goodbye"
+# end
+  
+  
+  
+
+
+
+
+
+
+# #compare input to a randomly generated number
+#   #print out statement if correct
+#   #print out statement if incorrectly
+# def check_guess (user_guess, num)
+  
+#   if user_guess == num
+#     puts "You guessed the correct number!"
+#   else
+#     puts "The computer guessed #{num}."
+#     exit_choice = gets.chomp
+#     if exit_choice.downcase == "exit"
+#       exit_program
+#     end  
+  
+# end
 
 
 
